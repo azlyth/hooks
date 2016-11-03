8c7a25f8dc9b8fdcf7189f8760cbe1052729f03f
Peter Valdez <peter@nycmesh.net>
2016-11-04 01:08:17 +0700

    Keep the server data across app usages

--- js/store.js
+++ js/store.js
@@ -1,6 +1,10 @@
 import { createStore } from 'redux';
+import { AsyncStorage } from 'react-native';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import reducer from './reducers';
 
-const store = createStore(reducer);
+
+const store = createStore(reducer, undefined, autoRehydrate());
+persistStore(store, {storage: AsyncStorage});
 
 export default store;
